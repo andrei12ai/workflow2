74abe4a64c90fbcee6a98dc02c2e0e793728a616
andrei12ai <111877984+andrei12ai@users.noreply.github.com>
2024-11-11 18:34:39 +0200

    Update workflow_analyzer2.py

--- workflow_analyzer2.py
+++ workflow_analyzer2.py
@@ -57,7 +57,7 @@ if uploaded_file is not None:
         # Split the label text into two lines
         label_parts = step["Name"].split(" - ")
         label = "\u2028".join(label_parts)
-        net.add_node(step["Id"], label=label, color=color, title=title, shape=shape, mass=10)
+        net.add_node(step["Id"], label="Line 1 <br> Line 2", color=color, title=title, shape=shape, mass=10)
 
     # PASS 2: Add edges only after all nodes have been added
     for step in dsl_data['Steps']:
